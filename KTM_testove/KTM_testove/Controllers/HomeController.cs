4d5dd28085e1abfb4f84ddc4b09a99c4d2bd4541
nazarsokal <93711302+nazarsokal@users.noreply.github.com>
2026-04-04 00:04:52 +0300

    Controller Create

--- KTM_testove/KTM_testove/Controllers/HomeController.cs
+++ KTM_testove/KTM_testove/Controllers/HomeController.cs
@@ -1,11 +1,32 @@
+using KTM_testove.Services.ServiceAbstractions;
 using Microsoft.AspNetCore.Mvc;
 
 namespace KTM_testove.Controllers;
 
+[ApiController]
 public class HomeController : ControllerBase
 {
-    public IActionResult Index()
+    private readonly IParsingService _parsingService;
+    private readonly IAiFeedbackService _aiiFeedbackService;
+
+    public HomeController(IParsingService parsingService, IAiFeedbackService aiiFeedbackService)
     {
-        return Ok("Hello World!");
+        _parsingService = parsingService;
+        _aiiFeedbackService = aiiFeedbackService;
+    }
+
+    [HttpPost]
+    [Route("/file/load")]
+    public async Task<IActionResult> LoadFile(IFormFile file)
+    {
+        var result = await _parsingService.Parse(file);
+        return Content(result, "application/json");
+    }
+
+    [HttpGet("ai/result/{result}")]
+    public async Task<IActionResult> GetFeedback(string result)
+    {
+        var aiResult = await _aiiFeedbackService.GetFeedbackAsync(result);
+        return Ok(aiResult);
     }
 }
--- KTM_testove/KTM_testove/Program.cs
+++ KTM_testove/KTM_testove/Program.cs
@@ -1,3 +1,6 @@
+using KTM_testove.Services;
+using KTM_testove.Services.ServiceAbstractions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +9,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddScoped<IParsingService, ParsingService>();
+builder.Services.AddHttpClient<IAiFeedbackService, AiFeedbackService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
