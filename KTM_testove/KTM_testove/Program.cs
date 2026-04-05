8a473aee7d737039f27f4c34a522f40b6f0ba57a
Nazar Sokalchuk <93711302+nazarsokal@users.noreply.github.com>
2026-04-05 20:46:04 +0300

    Update CORS policy origin URL
    Removed trailing slash from the origin URL.

--- KTM_testove/KTM_testove/Program.cs
+++ KTM_testove/KTM_testove/Program.cs
@@ -8,7 +8,7 @@ builder.Services.AddCors(options =>
     options.AddDefaultPolicy(policy =>
     {
         policy.WithOrigins(
-                "https://ktm-testove.vercel.app/",
+                "https://ktm-testove.vercel.app",
                 "https://vercel.com/nazarsokals-projects/ktm-testove/rbdaaXaHN8LubhperB3nnd6BVs5v",
                 "http://localhost:5173", // Залишаємо для локального Vite
                 "http://localhost:3000"  // На всякий випадок для локальних тестів
